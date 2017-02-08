6e94cea7df26bffcc800b916231ae4283f32b919
Alec <itsalbow@gmail.com>
2017-02-08 01:43:58 -0500

    added modular css

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,19 @@
  */
 
 const path = require('path');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: './src/index.html',
     filename: 'index.html',
     inject: 'body'
 });
 
+
+
 module.exports = {
-    entry: './src/index.js',
+    entry: './src',
     output: {
         path: path.resolve('dist'),
         filename: 'index_bundle.js'
@@ -22,7 +25,13 @@ module.exports = {
             test: /\.js$/,
             loader: 'babel-loader',
             exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
         }]
     },
-    plugins: [HtmlWebpackPluginConfig]
+    plugins: [
+        HtmlWebpackPluginConfig,
+        new ExtractTextPlugin('styles.css')
+    ]
 }
